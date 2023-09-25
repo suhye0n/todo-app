6adbcaff5670d798bd8db16c56249f2f2878eaf4
suhye0n <contact.suhyeon@gmail.com>
2023-09-25 09:15:25 +0900

    @NamedQuery, @Query 사용

---
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.model.TodoEntity;
+import com.example.demo.persistence.TodoRepository;
+
+@Service
+public class TodoService {
+	
+	@Autowired
+	private TodoRepository repository;
+
+	public String testService() {
+		TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
+		
+		repository.save(entity);
+		
+		TodoEntity savedEntity = repository.searchByUserId(entity.getUserId()).get(0);
+		return savedEntity.getUserId();
+	}
+}
