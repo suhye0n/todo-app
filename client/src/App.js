0b6a82400c52cc639cd271a42d56aea8a53c6163
suhye0n <contact.suhyeon@gmail.com>
2023-09-26 10:27:01 +0900

    React 이벤트 핸들러로 목록 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Todo from './Todo';
-import {Paper, List}from "@material-ui/core";
+import AddTodo from './AddTodo';
+import { Paper, List, Container } from "@material-ui/core";
 import './App.css';
 
 class App extends React.Component {
@@ -21,6 +22,15 @@ class App extends React.Component {
     };
   }
 
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length;
+    item.done = false;
+    thisItems.push(item);
+    this.setState({items:thisItems});
+    console.log("items:", this.state.items);
+  }
+
   render() {
     var todoItems = this.state.items.length > 0 && (
       <Paper style={{ margin: 16 }}>
@@ -31,10 +41,13 @@ class App extends React.Component {
         </List>
       </Paper>
     );
-    
+
     return (
       <div className='App'>
-        {todoItems}
+        <Container maxWidth='md'>
+          <AddTodo add={this.add} />
+          <div className='TodoList'>{todoItems}</div>
+          </Container>
       </div>
     )
   }
