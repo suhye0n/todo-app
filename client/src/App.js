93d5070a0c07f32437be325d9d33afe99c11d6a1
suhye0n <contact.suhyeon@gmail.com>
2023-09-26 09:56:46 +0900

    Props, state 사용

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Todo from './Todo';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: 0,
+          title: "Todo 1",
+          done: false
+        }, {
+          id: 1,
+          title: "Todo 2",
+          done: false
+        },
+      ],
+    };
+  }
+
+  render() {
+    var todoItems = this.state.items.map((item, idx) => (
+      <Todo item={item} key={item.id} />
+    ))
+    return (
+      <div className='App'>
+        {todoItems}
+      </div>
+    )
+  }
+}
+
+export default App;
