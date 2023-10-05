03a99e8b5d9dadac3964d246b59b0c058b2e7b1f
suhye0n <contact.suhyeon@gmail.com>
2023-10-05 09:08:12 +0900

    배포

--- client/src/app-config.js
+++ client/src/app-config.js
@@ -1,4 +1,4 @@
 const hostname = window?.location?.hostname;
 console.log("hostname", hostname);
 
-export const API_BASE_URL = hostname === "localhost" ? "http://localhost:8080" : undefined;
+export const API_BASE_URL = hostname === "localhost" ? "http://localhost:8080" : "172.30.123.88:8080";
